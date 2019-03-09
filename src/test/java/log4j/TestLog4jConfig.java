6428438a081987e3f227d258be0e850347e0a86f
MXuDong <2941884109@qq.com>
2019-03-09 22:25:22 +0800

    添加测试Log4j配置的测试类

---
+++ src/test/java/log4j/TestLog4jConfig.java
@@ -0,0 +1,13 @@
+package log4j;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : TestLog4jConfig
+ * Create Time : 22:25
+ * Create Date : 2019/3/9
+ * Project : WebFrameWork
+ */
+
+public class TestLog4jConfig {
+}
