4fb586bbf4e64c9413983580bafe956b5011e7ec
MXuDong <2941884109@qq.com>
2019-03-09 13:39:57 +0800

    Source Code

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : TestMain
+ * Create Time : 13:37
+ * Create Date : 2019/3/9
+ * Project : WebFrameWork
+ *
+ * 测试总类
+ */
+
+public class TestMain {
+
+    @Test
+    public void TestProgrameBegin(){
+        System.out.println("It's OK");
+    }
+}
